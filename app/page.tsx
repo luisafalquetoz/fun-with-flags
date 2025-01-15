1b62fa6542fd3fb819e78e02c4a1289c7efc5ac5
luisafalquetoz <luisa_fzandonade@hotmail.com>
2025-01-15 18:28:15 -0300

    get countries data from rest countries API

--- app/page.tsx
+++ app/page.tsx
@@ -1,51 +1,71 @@
-import { Card, Footer, Grid, Header } from "./components";
+'use client';
 
-const countries = [
-	{
-		id: 1,
-		country: "Brazil",
-		capital: "Brasília",
-		region: "South America",
-		population: "214300000",
-	},
-	{
-		id: 2,
-		country: "Japan",
-		capital: "Brasília",
-		region: "South America",
-		population: "214300000",
-	},
-	{
-		id: 3,
-		country: "United States",
-		capital: "Brasília",
-		region: "South America",
-		population: "214300000",
-	},
-	{
-		id: 4,
-		country: "Germany",
-		capital: "Brasília",
-		region: "South America",
-		population: "214300000",
-	},
-];
+import { useEffect, useState } from 'react';
+import { Card, Footer, Grid, Header } from './components';
+
+type Country = {
+	cca3: string;
+	flags: {
+		svg: string;
+	};
+	name: {
+		common: string;
+	};
+	capital: string[];
+	region: string;
+	population: number;
+};
 
 export default function Home() {
+	const [countries, setCountries] = useState<Country[]>([]);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState<string | null>(null);
+
+	useEffect(() => {
+		const fetchCountries = async () => {
+			try {
+				const response = await fetch(
+					'https://restcountries.com/v3.1/all?fields=cca3,flags,name,capital,region,population',
+				);
+				const data = await response.json();
+				setCountries(data);
+			} catch (error) {
+				setError('Failed to fetch data');
+				console.error(error);
+			} finally {
+				setLoading(false);
+			}
+		};
+		fetchCountries();
+	}, []);
+
+	if (loading) return <div>Loading...</div>;
+	if (error) return <div>(error)</div>;
+
 	return (
 		<>
 			<Header />
 			<main className="flex-1">
 				<Grid>
-					{countries.map(({ id, country, capital, region, population }) => (
-						<Card
-							key={id}
-							country={country}
-							capital={capital}
-							region={region}
-							population={population}
-						/>
-					))}
+					{countries.map(
+						({ cca3, flags, name, capital, region, population }, index) => {
+							const { svg: flag } = flags ?? {};
+							const { common: countryName } = name ?? {};
+							const [capitalName] = capital ?? [];
+
+							return (
+								<Card
+									key={cca3}
+									index={index}
+									flag={flag}
+									name={countryName}
+									capital={capitalName}
+									region={region}
+									population={population}
+								/>
+							);
+						},
+					)}
 				</Grid>
 			</main>
 			<Footer />

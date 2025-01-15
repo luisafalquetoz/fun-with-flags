eb26966efeeec745a9f2e6901e327ce8f99549bb
luisafalquetoz <luisa_fzandonade@hotmail.com>
2025-01-15 16:49:56 -0300

    add Card and Grid components and show the cards list

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,53 @@
-import { Footer, Header } from "./components";
+import { Card, Footer, Grid, Header } from "./components";
+
+const countries = [
+	{
+		id: 1,
+		country: "Brazil",
+		capital: "Brasília",
+		region: "South America",
+		population: "214300000",
+	},
+	{
+		id: 2,
+		country: "Japan",
+		capital: "Brasília",
+		region: "South America",
+		population: "214300000",
+	},
+	{
+		id: 3,
+		country: "United States",
+		capital: "Brasília",
+		region: "South America",
+		population: "214300000",
+	},
+	{
+		id: 4,
+		country: "Germany",
+		capital: "Brasília",
+		region: "South America",
+		population: "214300000",
+	},
+];
 
 export default function Home() {
 	return (
 		<>
 			<Header />
-			<main className="flex-1">Main</main>
+			<main className="flex-1">
+				<Grid>
+					{countries.map(({ id, country, capital, region, population }) => (
+						<Card
+							key={id}
+							country={country}
+							capital={capital}
+							region={region}
+							population={population}
+						/>
+					))}
+				</Grid>
+			</main>
 			<Footer />
 		</>
 	);
